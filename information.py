e61763bce01ced0e9e9d4a34fff81ac87141d003
Dasha <plotnikova.dm@phystech.edu>
2022-04-25 00:43:09 +0300

    add random functions

--- information.py
+++ information.py
@@ -1,12 +1,40 @@
 import pandas as pd
+import random as rd
 
 
-def get_information(row_number, data):
-    answer = [data['ShortName'][row_number],
+def get_information(row_number, data):  # get information about certain place
+    answer = [data['CommonName'][row_number],
               data['ObjectAddress'][row_number][0]['Address'],
               data['WebSite'][row_number],
               'Часы работы:']
-    working_hours = [data['WorkingHours'][0][i]['DayWeek'] + ' ' + data['WorkingHours'][0][i]['WorkHours'] for i in range(7)]
+    working_hours = [data['WorkingHours'][row_number][i]['DayWeek'] + ' ' + data['WorkingHours'][row_number][i]['WorkHours'] for i in range(7)]
     answer = answer + working_hours
+    answer.append(data['ClarificationOfWorkingHours'][row_number])
+    answer = list(map(str, filter(lambda x: x != 'nan', answer)))
     return answer
+
+
+def get_random_places(data, number=1):  # returns information about random places
+    number_of_places = min(number, len(data.index) - 1)
+    answer = [0] * number_of_places
+    for i in range(number_of_places):
+        place = rd.randint(0, len(data.index) - 1)
+        while place in answer:
+            place = rd.randint(0, len(data.index) - 1)
+        answer[i] = place
+    return [get_information(answer[i], data) for i in range(number_of_places)]
+
+
+def get_places_in_certain_area(main_data, area_name, number):  # returns numbers of random places in certain area
+    data_with_areas = main_data
+    data_with_areas['Area'] = [main_data['ObjectAddress'][i][0]['AdmArea'] for i in range(len(main_data.index))]
+    data = pd.DataFrame(data_with_areas[data_with_areas['Area'] == area_name])  #
+    number_of_places = min(number, len(data.index))
+    answer = [0] * number_of_places
+    for i in range(number_of_places):
+        place = rd.randint(0, len(data.index) - 1)
+        while place in answer:
+            place = rd.randint(0, len(data.index) - 1)
+        answer[i] = place
+    return [get_information(answer[i], data) for i in range(number_of_places)]
 
