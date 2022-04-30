b30c5c37b96db2ccf3552952b5028e3c02335dca
Dasha <plotnikova.dm@phystech.edu>
2022-04-30 23:15:34 +0300

    update geocoder

--- geocoder.py
+++ geocoder.py
@@ -2,9 +2,6 @@ import requests
 
 def find_coordinates(adress): # строка формата 'город Москва, улица Арбат, дом 55/32'
     list_adress = adress.split(', ')
-    list_adress[0] = list_adress[0][6:]
-    list_adress[1] = list_adress[1][6:]
-    list_adress[2] = list_adress[2][4:]
     adress = '+'.join(list_adress)
     geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={adress}&format=json"
     response = requests.get(geocoder_request)
