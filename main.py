6d07fffa96d84865b2876e80e364d8f9e7870c09
Dasha <plotnikova.dm@phystech.edu>
2022-04-30 15:01:22 +0300

    main file

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
-from information import get_information
+from information import get_information, get_random_places, get_places_in_certain_area
 
 museums = pd.read_json('data/museums.json')
-print('\n'.join(get_information(5, museums)))
+answer = get_random_places(museums, 5)
+for i in range(5):
+    print('\n'.join(answer[i]))
+
+# museums = pd.read_json('data/museums.json')
+# answer = get_places_in_certain_area(museums, 'Юго-Восточный административный округ', 5)
+# for i in range(5):
+#     print('\n'.join(answer[i]))
