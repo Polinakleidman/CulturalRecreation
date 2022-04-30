8249982704e308391e5198dd984d7561eef86c3f
Dasha <plotnikova.dm@phystech.edu>
2022-04-30 20:41:46 +0300

    area func

--- information.py
+++ information.py
@@ -5,7 +5,7 @@ import random as rd
 def get_information(row_number, data):  # get information about certain place
     answer = [data['CommonName'][row_number],
               data['ObjectAddress'][row_number][0]['Address'],
-              data['WebSite'][row_number],
+              data['WebSite'][row_number], '', ''
               'Часы работы:']
     working_hours = [
         data['WorkingHours'][row_number][i]['DayWeek'] + ' ' + data['WorkingHours'][row_number][i]['WorkHours'] for i in
@@ -36,7 +36,7 @@ def get_information_about_certain_place(data, name):
         flag = True
         temp_data = temp_data[temp_data['Name'].str.contains(name1)]
     if not flag:
-        return ['Мы не поняли запрос, поэтому подобрали вам рандомное место:\n'] + get_information(rd.randint(0, len(temp_data.index) - 1), data)
+        return [['Мы не поняли запрос, поэтому подобрали вам рандомное место:\n']] + get_information(rd.randint(0, len(temp_data.index) - 1), data)
     if len(temp_data.index > 0):
         ind = temp_data.index[0]
         return [get_information(ind, data)]
@@ -44,15 +44,32 @@ def get_information_about_certain_place(data, name):
         return ['Место не найдено :(']
 
 
-def get_places_in_certain_area(main_data, area_name, number):  # returns numbers of random places in certain area
-    data_with_areas = main_data
-    data_with_areas['Area'] = [main_data['ObjectAddress'][i][0]['AdmArea'] for i in range(len(main_data.index))]
-    data = pd.DataFrame(data_with_areas[data_with_areas['Area'] == area_name])  #
-    number_of_places = min(number, len(data.index))
-    answer = [0] * number_of_places
-    for i in range(number_of_places):
-        place = rd.randint(0, len(data.index) - 1)
-        while place in answer:
-            place = rd.randint(0, len(data.index) - 1)
-        answer[i] = place
-    return [get_information(answer[i], data) for i in range(number_of_places)]
+# def get_places_in_certain_area(main_data, area_name, number):  # returns numbers of random places in certain area
+#     data_with_areas = main_data
+#     data_with_areas['Area'] = [main_data['ObjectAddress'][i][0]['AdmArea'] for i in range(len(main_data.index))]
+#     data = pd.DataFrame(data_with_areas[data_with_areas['Area'] == area_name])  #
+#     number_of_places = min(number, len(data.index))
+#     answer = [0] * number_of_places
+#     for i in range(number_of_places):
+#         place = rd.randint(0, len(data.index) - 1)
+#         while place in answer:
+#             place = rd.randint(0, len(data.index) - 1)
+#         answer[i] = place
+#     return [get_information(answer[i], data) for i in range(number_of_places)]
+
+
+def get_places_in_certain_area(data, name):
+    temp_data = data
+    temp_data['Area'] = [data['ObjectAddress'][i][0]['AdmArea'].lower() for i in range(len(data.index))]
+    temp_data = data
+    flag = False
+    for name1 in list(filter(lambda x: (x.isalpha() and len(x) > 2) or x.isdigit(), name.lower().split())):
+        flag = True
+        temp_data = temp_data[temp_data['Area'].str.contains(name1)]
+    if not flag:
+        return [['Мы не поняли запрос, поэтому подобрали вам рандомное место:\n']] + get_information(rd.randint(0, len(temp_data.index) - 1), data)
+    if len(temp_data.index > 0):
+        ind = temp_data.index[0]
+        return [get_information(ind, data)]
+    else:
+        return ['Место не найдено :(']
